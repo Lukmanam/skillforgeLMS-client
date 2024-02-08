ad88e8d00422135d2ecdd3d14ec2dc426548ee30
Lukmanam <lukkuptrklm2000@gmail.com>
2024-02-08 14:27:31 +0530

    changes for courselist component route

--- src/pages/studentPages/StudentHome.jsx
+++ src/pages/studentPages/StudentHome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import StudentNavbar from "../../components/studentComponent/StudentNavbar";
 import Banner from "../../components/studentComponent/Banner";
-import CourseList from "../../components/studentComponent/courseListComponent";
+import CourseList from "../../components/studentComponent/CourseListComponent";
 
 
 const StudentHome=()=>{
