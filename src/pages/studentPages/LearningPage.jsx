5a3e4df84b6d3cffceb9052b4016b2c8539005e1
Lukmanam <lukkuptrklm2000@gmail.com>
2024-02-29 18:40:25 +0530

    ui change

--- src/pages/studentPages/LearningPage.jsx
+++ src/pages/studentPages/LearningPage.jsx
@@ -97,41 +97,45 @@ const LearningPage = () => {
           <div className="container mx-auto my-8 bg-gray-800 rounded-md p-4">
             <div className="flex flex-wrap items-center">
               {/* Left Part - Video Player Skeleton */}
-              <div className="md:w-full lg:w-1/3 bg-gray-700 rounded-md p-4">
+              <div className="md:w-full lg:w-2/3 bg-gray-700 rounded-md p-4">
                 {/* Skeleton for video player */}
-                <div className="w-full h-64 bg-gray-600 rounded-md"></div>
+                <div className="w-full h-96 bg-gray-600 rounded-md"></div>
               </div>
 
               {/* Right Part - Course Details Skeleton */}
-              <div className="w-full md:1/2 lg:w-2/3 p-4">
+              <div className="w-full md:1/2 lg:w-1/3 p-4">
                 {/* Skeleton for course details */}
-                <div className="mb-3 h-8 bg-gray-600 rounded-md"></div>
-                <div className="mb-4 h-6 bg-gray-600 rounded-md"></div>
+                <div className="mb-4 h-16 bg-gray-600 rounded-md"></div>
+                <div className="mb-4 h-16 bg-gray-600 rounded-md"></div>
+                <div className="mb-4 h-16 bg-gray-600 rounded-md"></div>
+                <div className="mb-4 h-16 bg-gray-600 rounded-md"></div>
+                <div className="mb-4 h-16 bg-gray-600 rounded-md"></div>
+                <div className="mb-4 h-16 bg-gray-600 rounded-md"></div>
 
                 <div className="grid grid-cols-1 md:grid-cols-2">
                   <div className="w-full mb-2">
                     {/* Skeleton for level */}
-                    <div className="flex flex-row items-center mb-1">
+                    {/* <div className="flex flex-row items-center mb-1">
                       <div className="text-gray-400 rounded-md font-mono text-md font-thin h-4 w-24 bg-gray-600"></div>
-                    </div>
+                    </div> */}
 
                     {/* Skeleton for price */}
-                    <div className="flex flex-row items-center mb-1">
+                    {/* <div className="flex flex-row items-center mb-1">
                       <div className="text-gray-400 rounded-md font-mono text-md font-thin h-4 w-24 bg-gray-600"></div>
-                    </div>
+                    </div> */}
 
                     {/* Skeleton for instructor */}
-                    <div className="flex flex-row items-center mb-1">
+                    {/* <div className="flex flex-row items-center mb-1">
                       <div className="text-gray-400 rounded-md text-md font-mono font-thin h-4 w-24 bg-gray-600"></div>
-                    </div>
+                    </div> */}
                   </div>
 
-                  <div className="w-full h-full flex items-end">
-                    <div>
+                  {/* <div className="w-full h-full flex items-end"> */}
+                    {/* <div> */}
                       {/* Skeleton for enroll button */}
-                      <div className="bg-gray-600 text-sm text-white text-center font-semibold p-3 rounded-md h-10 w-32"></div>
-                    </div>
-                  </div>
+                      {/* <div className="bg-gray-600 text-sm text-white text-center font-semibold p-3 rounded-md h-10 w-32"></div> */}
+                    {/* </div> */}
+                  {/* </div> */}
                 </div>
               </div>
             </div>
