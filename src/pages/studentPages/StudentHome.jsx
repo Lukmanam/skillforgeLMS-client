d053ca8baa9a5233fc0d76348afdb4a2d1cca8c6
Lukmanam <lukkuptrklm2000@gmail.com>
2024-03-13 12:01:18 +0530

    fixed resolve file error

--- src/pages/studentPages/StudentHome.jsx
+++ src/pages/studentPages/StudentHome.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import StudentNavbar from "../../components/studentComponent/StudentNavbar";
 import Banner from "../../components/studentComponent/Banner";
 import CourseList from "../../components/studentComponent/CourseListComponent";
-import StudentFooter from "../../components/studentComponent/studentFooter";
+import StudentFooter from "../../components/studentComponent/StudentFooter";
 
 
 const StudentHome=()=>{
